bb3108dd9ae489d049092c1c0382abd1530f54ce
viper660 <78082517+viper660@users.noreply.github.com>
2021-05-08 00:28:44 +0530

    ff

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const discord = require("discord.js");
-const client = new discord.Client()
 const { prefix, ServerID } = require("./config.json")
 const config = require('./config.json');
 const client = new discord.Client({
